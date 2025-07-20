f27ec827058325d21177f601894e561bf99f87ae
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-20 23:31:40 +0300

    feat: use cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix("api/v1");
+
+	app.use(cookieParser());
 
 	app.useGlobalPipes(
 		new ValidationPipe({
