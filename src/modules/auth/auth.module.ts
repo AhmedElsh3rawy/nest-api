dabf6a95a819903c710af83cbd9cd6e863945fa7
Ahmed <ahmedelsharawy117@gmail.com>
2025-07-23 23:58:03 +0300

    chore: import local strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,14 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UsersModule } from "../users/users.module";
 import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-	imports: [UsersModule, JwtModule],
+	imports: [UsersModule, JwtModule, PassportModule],
 	controllers: [AuthController],
-	providers: [AuthService],
+	providers: [AuthService, LocalStrategy],
+	exports: [AuthService],
 })
 export class AuthModule {}
